e6c30317adf9a2fe0c8c6cca11b51f1370f3e54b
pfg <omegariisthebest@gmail.com>
2020-05-22 01:31:34 -0700

    huh

--- src/workaround.h
+++ src/workaround.h
@@ -1,23 +1,19 @@
 #include <raylib.h>
 
+#ifdef workaround_implementation
+	#define IMPL(body) body
+#else
+	#define IMPL(body) ;
+#endif
+
 void _wDrawTextureV(
 	const Texture2D* texture, const Vector2* position, const Color* tint
-)
-#ifdef workaround_implementation
-{
+) IMPL({
 	DrawTextureV(*texture, *position, *tint);
-}
-#else
-;
-#endif
+})
 
 void _wGetScreenToWorld2D(
 	const Vector2* position, const Camera2D* camera, Vector2* out
-)
-#ifdef workaround_implementation
-{
+) IMPL({
 	*out = GetScreenToWorld2D(*position, *camera);
-}
-#else
-;
-#endif
+})
