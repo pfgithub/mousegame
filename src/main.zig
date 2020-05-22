9ac52415cb5c59ef242322524046dce133135065
pfg <omegariisthebest@gmail.com>
2020-05-22 01:45:47 -0700

    I think mouse movements are going to need to be real physics because otherwise everything will be too easy. maybe.

--- src/main.zig
+++ src/main.zig
@@ -14,11 +14,12 @@ pub fn hex(comptime color: u24) ray.Color {
 
 const Game = struct {
     cursor: ray.Vector2 = ray.Vector2{ .x = 200, .y = 100 },
+    cursorVelocity: ray.Vector2 = ray.Vector2{ .x = 0, .y = 0 },
 };
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 400;
+    const screenWidth = 1920;
+    const screenHeight = 1080;
 
     ray.InitWindow(screenWidth, screenHeight, "window");
     defer ray.CloseWindow();
@@ -35,11 +36,30 @@ pub fn main() !void {
     camera.offset = .{ .x = 0, .y = 0 };
     camera.rotation = 0;
     camera.zoom = 2;
+    var previousMousePos: ray.Vector2 = .{ .x = 0, .y = 0 };
 
-    const game: Game = .{};
+    var game: Game = .{};
 
     while (!ray.WindowShouldClose()) {
         const delta = ray.GetFrameTime();
+
+        const currentMousePos = ray.WGetScreenToWorld2D(GetMousePosition(), camera);
+        const mousePos: ray.Vector2 = .{
+            .x = currentMousePos.x - previousMousePos.x,
+            .y = currentMousePos.y - previousMousePos.y,
+        };
+        previousMousePos = currentMousePos;
+
+        game.cursor.x += std.math.clamp(mousePos.x, -10_000 * delta, 10_000 * delta);
+        game.cursor.y += std.math.clamp(mousePos.y, -10_000 * delta, 10_000 * delta);
+
+        game.cursorVelocity.y += 1 * delta;
+        game.cursor.x += game.cursorVelocity.x;
+        game.cursor.y += game.cursorVelocity.y;
+        if (game.cursor.y > 500) {
+            game.cursor.y = 500;
+            game.cursorVelocity.y = 0;
+        }
 
         ray.BeginDrawing();
         defer ray.EndDrawing();
@@ -50,8 +70,5 @@ pub fn main() !void {
         defer ray.EndMode2D();
 
         ray.WDrawTextureV(mouse, game.cursor, hex(0xFFFFFF));
-
-        const mousePos = ray.WGetScreenToWorld2D(GetMousePosition(), camera);
-        std.debug.warn("mpos: {}\n", .{mousePos});
     }
 }
